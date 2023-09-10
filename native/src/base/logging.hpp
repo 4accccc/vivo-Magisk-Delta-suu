f325049e955a48fdcf2f86a0817e74d06516d43f
4accccc <2594696244@qq.com>
2023-09-10 12:46:45 +0800

    0fe46c5asuu-delta

---
+++ native/src/base/logging.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cerrno>
+#include <cstdarg>
+
+extern bool logging_muted;
+
+void LOGD(const char *fmt, ...) __printflike(1, 2);
+void LOGI(const char *fmt, ...) __printflike(1, 2);
+void LOGW(const char *fmt, ...) __printflike(1, 2);
+void LOGE(const char *fmt, ...) __printflike(1, 2);
+#define PLOGE(fmt, args...) LOGE(fmt " failed with %d: %s\n", ##args, errno, std::strerror(errno))
---
+++ native/src/zygisk/var.hpp
@@ -0,0 +1,3 @@
+#define NATIVE_BRIDGE_PROP "ro.dalvik.vm.native.bridge"
+#define LOADER_LIB "libzygisk-ld.so"
+#define ZYGISK_LIB "libzygisk.so"
